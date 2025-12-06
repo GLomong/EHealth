2b1469102658940226ecc70956fbef9b94d5d880
Elisa160601 <elisa.madonia@mail.polimi.it>
2025-12-06 13:19:12 +0100

    Prove gioco ponte

---
+++ Assets/Cose Ponte/TestButtons.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TestButtons : MonoBehaviour
+{
+    public void DebugX1()
+    {
+        Debug.Log("Hai premuto X1!");
+    }
+
+    public void DebugX2()
+    {
+        Debug.Log("Hai premuto X2!");
+    }
+
+    public void DebugX3()
+    {
+        Debug.Log("Hai premuto X3!");
+    }
+}
+
