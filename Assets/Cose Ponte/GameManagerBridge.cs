335810d5fd83d62239f93ad8f5c4a4ffdd562a12
Elisa160601 <elisa.madonia@mail.polimi.it>
2025-12-06 16:19:19 +0100

    Gioco Ponte finito
    Ho completato il gioco del ponte, con punteggio, tempo, caduta, suoni e animazioni. Mi manca da mettere un pannello finale e creare la transizione con la città.

---
+++ Assets/Cose Ponte/GameManagerBridge.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManagerBridge : MonoBehaviour
+{
+    public static GameManagerBridge instance;
+
+    [Header("UI")]
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timeText;
+
+    [Header("Score Settings")]
+    public int score = 0;
+    public int puntiPerAsse = 10;
+    public int bonusX2 = 5;
+    public int bonusX3 = 10;
+    public int penalitaCaduta = 20;
+
+    private float timer = 0f;
+
+    // Stati
+    private bool timerAttivo = false;
+    public bool scoreActive = true;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        if (timerAttivo)
+        {
+            timer += Time.deltaTime;
+            timeText.text = "TIME: " + timer.ToString("F2");
+        }
+    }
+
+    // ------------------------------ TIMER ------------------------------
+
+    public void StartTimer()
+    {
+        timerAttivo = true;
+    }
+
+    public void StopTimer()
+    {
+        timerAttivo = false;
+    }
+
+    // ------------------------------ SCORE ------------------------------
+
+    public void AggiungiPunti(int quantiAssi)
+    {
+        if (!scoreActive) return;
+
+        score += puntiPerAsse;
+
+        if (quantiAssi == 2) score += bonusX2;
+        if (quantiAssi == 3) score += bonusX3;
+
+        AggiornaScoreUI();
+    }
+
+    public void PenalitaCaduta()
+    {
+        if (!scoreActive) return;
+
+        score -= penalitaCaduta;
+        if (score < 0) score = 0;
+
+        AggiornaScoreUI();
+    }
+
+    public void StopScore()
+    {
+        scoreActive = false;
+    }
+
+    void AggiornaScoreUI()
+    {
+        scoreText.text = "SCORE: " + score;
+    }
+}
+
